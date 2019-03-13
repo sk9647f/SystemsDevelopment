3f0d01212c5b152c74ddada8d29cc9e9cbf0e79a
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-03-13 12:25:16 +0000

    Update Form1.Designer.cs

--- TicketBookingSystem/TicketBookingSystem/Form1.Designer.cs
+++ TicketBookingSystem/TicketBookingSystem/Form1.Designer.cs
@@ -168,6 +168,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "What\'s On";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
