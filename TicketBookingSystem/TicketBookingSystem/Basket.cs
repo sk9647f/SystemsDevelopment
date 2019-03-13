17061da0946cdbe5c84ce1fc0c616012e39b163b
ublomas <47817943+ublomas@users.noreply.github.com>
2019-03-13 12:26:50 +0000

    Update Basket.cs

--- TicketBookingSystem/TicketBookingSystem/Basket.cs
+++ TicketBookingSystem/TicketBookingSystem/Basket.cs
@@ -1,21 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace TicketBookingSystem
 {
-    class Basket
+    abstract class Basket
+    {
+        double basketTotal;
+        int cost, quantity;
+        string play, user;
+        DateTime date;
+        public abstract void AddToOrder(string user, string play, int cost, int qauntity, DateTime date, string ticketType);
+        public abstract void RemoveFromBasket();
+    }
+
+    class ChosenPlays : Basket
     {
 
-        double basketTotal;
-        int ticketsInBasket;
-        List<string> basketItems;
-        string playName;
-
-        public void DisplayCurrentTotal()
+        public override void AddToOrder(string user, string play, int cost, int quantity, DateTime date, string ticketType)
         {
+
+            //id = customerID;
+            //add = address;
+
+            //needs to write to database 
+            string connString;
+            connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = C:\Users\user\Desktop\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
+            OleDbConnection myConnection = new OleDbConnection(connString);
+
+            OleDbCommand myCommand = new OleDbCommand("INSERT INTO Basket (UserID, PlayChosen, Price, Quantity, PlayDate, TicketType) VALUES (?, ?, ?, ?, ?, ?)", myConnection);
+            
+            myCommand.Parameters.AddWithValue("@UserID", user);
+            myCommand.Parameters.AddWithValue("@PlayChosen", play);
+            myCommand.Parameters.AddWithValue("@Price", cost);
+            myCommand.Parameters.AddWithValue("@Quantity", quantity);
+            myCommand.Parameters.AddWithValue("@PlayDate", date);
+            myCommand.Parameters.AddWithValue("@TicketType", ticketType);
+
+            myConnection.Open();
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
 
         }
 
@@ -35,25 +63,10 @@ namespace TicketBookingSystem
         }
 
         public void AddToBasket()
-        {
-
-
-            for (int i = 0; i < basketItems.Count; i++)
-            {
-                /* if the item is already in the list
-                if (basketItems[i]. == playName)
-                {
-                    basketItems[i].UpdateShoppingBasketList(ticketsInBasket, basketTotal);
-                    return;
-                }*/
-            }
-            
-
-
-
+        { 
         }
 
-        public void RemoveFromBasket()
+        public override void RemoveFromBasket()
         {
 
         }
