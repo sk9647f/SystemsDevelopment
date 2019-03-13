f40ac052724316a06ffc27a1bba4ee8fc50dd59c
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-03-13 12:15:34 +0000

    Add files via upload

---
+++ TicketBookingSystem/TicketBookingSystem/PlaysForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TicketBookingSystem
+{
+    partial class Plays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddPlay1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TitleL = new System.Windows.Forms.Label();
+            this.Titlet = new System.Windows.Forms.TextBox();
+            this.Genrel = new System.Windows.Forms.Label();
+            this.Descriptionl = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Descriptiont = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.InputNewPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddPlay1
+            // 
+            this.AddPlay1.Location = new System.Drawing.Point(387, 330);
+            this.AddPlay1.Name = "AddPlay1";
+            this.AddPlay1.Size = new System.Drawing.Size(75, 23);
+            this.AddPlay1.TabIndex = 0;
+            this.AddPlay1.Text = "Add a play";
+            this.AddPlay1.UseVisualStyleBackColor = true;
+            this.AddPlay1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TitleL
+            // 
+            this.TitleL.AutoSize = true;
+            this.TitleL.Location = new System.Drawing.Point(29, 34);
+            this.TitleL.Name = "TitleL";
+            this.TitleL.Size = new System.Drawing.Size(27, 13);
+            this.TitleL.TabIndex = 2;
+            this.TitleL.Text = "Title";
+            // 
+            // Titlet
+            // 
+            this.Titlet.Location = new System.Drawing.Point(32, 50);
+            this.Titlet.Name = "Titlet";
+            this.Titlet.Size = new System.Drawing.Size(171, 20);
+            this.Titlet.TabIndex = 3;
+            // 
+            // Genrel
+            // 
+            this.Genrel.AutoSize = true;
+            this.Genrel.Location = new System.Drawing.Point(29, 85);
+            this.Genrel.Name = "Genrel";
+            this.Genrel.Size = new System.Drawing.Size(36, 13);
+            this.Genrel.TabIndex = 4;
+            this.Genrel.Text = "Genre";
+            // 
+            // Descriptionl
+            // 
+            this.Descriptionl.AutoSize = true;
+            this.Descriptionl.Location = new System.Drawing.Point(29, 131);
+            this.Descriptionl.Name = "Descriptionl";
+            this.Descriptionl.Size = new System.Drawing.Size(60, 13);
+            this.Descriptionl.TabIndex = 5;
+            this.Descriptionl.Text = "Description";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // Descriptiont
+            // 
+            this.Descriptiont.Location = new System.Drawing.Point(32, 157);
+            this.Descriptiont.Name = "Descriptiont";
+            this.Descriptiont.Size = new System.Drawing.Size(171, 20);
+            this.Descriptiont.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(32, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(171, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(32, 242);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(171, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(32, 287);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(171, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(32, 332);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(171, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // InputNewPlay
+            // 
+            this.InputNewPlay.Location = new System.Drawing.Point(209, 330);
+            this.InputNewPlay.Name = "InputNewPlay";
+            this.InputNewPlay.Size = new System.Drawing.Size(110, 23);
+            this.InputNewPlay.TabIndex = 16;
+            this.InputNewPlay.Text = "Input Play Details";
+            this.InputNewPlay.UseVisualStyleBackColor = true;
+            // 
+            // Plays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 365);
+            this.Controls.Add(this.InputNewPlay);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Descriptiont);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Descriptionl);
+            this.Controls.Add(this.Genrel);
+            this.Controls.Add(this.Titlet);
+            this.Controls.Add(this.TitleL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddPlay1);
+            this.Name = "Plays";
+            this.Text = "Plays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddPlay1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TitleL;
+        private System.Windows.Forms.TextBox Titlet;
+        private System.Windows.Forms.Label Genrel;
+        private System.Windows.Forms.Label Descriptionl;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Descriptiont;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button InputNewPlay;
+    }
+}
