dc3d4095ff20fdec800d324b1a3a1b9b1ff167da
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-03-13 12:37:28 +0000

    Add files via upload

---
+++ TicketBookingSystem/TicketBookingSystem/PlaysForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace TicketBookingSystem
+{
+    partial class PlaysForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddPlay1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TitleL = new System.Windows.Forms.Label();
+            this.Titlet = new System.Windows.Forms.TextBox();
+            this.Genrel = new System.Windows.Forms.Label();
+            this.Descriptionl = new System.Windows.Forms.Label();
+            this.Genret = new System.Windows.Forms.TextBox();
+            this.Datel = new System.Windows.Forms.Label();
+            this.Timel = new System.Windows.Forms.Label();
+            this.TicketsAval = new System.Windows.Forms.Label();
+            this.TicketQuanl = new System.Windows.Forms.Label();
+            this.Descriptiont = new System.Windows.Forms.TextBox();
+            this.Datet = new System.Windows.Forms.TextBox();
+            this.Timet = new System.Windows.Forms.TextBox();
+            this.TicketsAvat = new System.Windows.Forms.TextBox();
+            this.TicketQuant = new System.Windows.Forms.TextBox();
+            this.InputNewPlay = new System.Windows.Forms.Button();
+            this.AddBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddPlay1
+            // 
+            this.AddPlay1.Location = new System.Drawing.Point(387, 330);
+            this.AddPlay1.Name = "AddPlay1";
+            this.AddPlay1.Size = new System.Drawing.Size(75, 23);
+            this.AddPlay1.TabIndex = 0;
+            this.AddPlay1.Text = "Add a play";
+            this.AddPlay1.UseVisualStyleBackColor = true;
+            this.AddPlay1.Click += new System.EventHandler(this.AddPlay1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TitleL
+            // 
+            this.TitleL.AutoSize = true;
+            this.TitleL.Location = new System.Drawing.Point(29, 34);
+            this.TitleL.Name = "TitleL";
+            this.TitleL.Size = new System.Drawing.Size(27, 13);
+            this.TitleL.TabIndex = 2;
+            this.TitleL.Text = "Title";
+            // 
+            // Titlet
+            // 
+            this.Titlet.Location = new System.Drawing.Point(32, 50);
+            this.Titlet.Name = "Titlet";
+            this.Titlet.Size = new System.Drawing.Size(171, 20);
+            this.Titlet.TabIndex = 3;
+            // 
+            // Genrel
+            // 
+            this.Genrel.AutoSize = true;
+            this.Genrel.Location = new System.Drawing.Point(29, 85);
+            this.Genrel.Name = "Genrel";
+            this.Genrel.Size = new System.Drawing.Size(36, 13);
+            this.Genrel.TabIndex = 4;
+            this.Genrel.Text = "Genre";
+            // 
+            // Descriptionl
+            // 
+            this.Descriptionl.AutoSize = true;
+            this.Descriptionl.Location = new System.Drawing.Point(29, 131);
+            this.Descriptionl.Name = "Descriptionl";
+            this.Descriptionl.Size = new System.Drawing.Size(60, 13);
+            this.Descriptionl.TabIndex = 5;
+            this.Descriptionl.Text = "Description";
+            // 
+            // Genret
+            // 
+            this.Genret.Location = new System.Drawing.Point(32, 99);
+            this.Genret.Name = "Genret";
+            this.Genret.Size = new System.Drawing.Size(171, 20);
+            this.Genret.TabIndex = 6;
+            // 
+            // Datel
+            // 
+            this.Datel.AutoSize = true;
+            this.Datel.Location = new System.Drawing.Point(29, 180);
+            this.Datel.Name = "Datel";
+            this.Datel.Size = new System.Drawing.Size(30, 13);
+            this.Datel.TabIndex = 7;
+            this.Datel.Text = "Date";
+            // 
+            // Timel
+            // 
+            this.Timel.AutoSize = true;
+            this.Timel.Location = new System.Drawing.Point(29, 226);
+            this.Timel.Name = "Timel";
+            this.Timel.Size = new System.Drawing.Size(30, 13);
+            this.Timel.TabIndex = 8;
+            this.Timel.Text = "Time";
+            // 
+            // TicketsAval
+            // 
+            this.TicketsAval.AutoSize = true;
+            this.TicketsAval.Location = new System.Drawing.Point(29, 271);
+            this.TicketsAval.Name = "TicketsAval";
+            this.TicketsAval.Size = new System.Drawing.Size(88, 13);
+            this.TicketsAval.TabIndex = 9;
+            this.TicketsAval.Text = "Tickets Available";
+            // 
+            // TicketQuanl
+            // 
+            this.TicketQuanl.AutoSize = true;
+            this.TicketQuanl.Location = new System.Drawing.Point(29, 316);
+            this.TicketQuanl.Name = "TicketQuanl";
+            this.TicketQuanl.Size = new System.Drawing.Size(84, 13);
+            this.TicketQuanl.TabIndex = 10;
+            this.TicketQuanl.Text = "Tickets Quantity";
+            // 
+            // Descriptiont
+            // 
+            this.Descriptiont.Location = new System.Drawing.Point(32, 157);
+            this.Descriptiont.Name = "Descriptiont";
+            this.Descriptiont.Size = new System.Drawing.Size(171, 20);
+            this.Descriptiont.TabIndex = 11;
+            // 
+            // Datet
+            // 
+            this.Datet.Location = new System.Drawing.Point(32, 203);
+            this.Datet.Name = "Datet";
+            this.Datet.Size = new System.Drawing.Size(171, 20);
+            this.Datet.TabIndex = 12;
+            // 
+            // Timet
+            // 
+            this.Timet.Location = new System.Drawing.Point(32, 242);
+            this.Timet.Name = "Timet";
+            this.Timet.Size = new System.Drawing.Size(171, 20);
+            this.Timet.TabIndex = 13;
+            // 
+            // TicketsAvat
+            // 
+            this.TicketsAvat.Location = new System.Drawing.Point(32, 287);
+            this.TicketsAvat.Name = "TicketsAvat";
+            this.TicketsAvat.Size = new System.Drawing.Size(171, 20);
+            this.TicketsAvat.TabIndex = 14;
+            // 
+            // TicketQuant
+            // 
+            this.TicketQuant.Location = new System.Drawing.Point(32, 332);
+            this.TicketQuant.Name = "TicketQuant";
+            this.TicketQuant.Size = new System.Drawing.Size(171, 20);
+            this.TicketQuant.TabIndex = 15;
+            // 
+            // InputNewPlay
+            // 
+            this.InputNewPlay.Location = new System.Drawing.Point(209, 330);
+            this.InputNewPlay.Name = "InputNewPlay";
+            this.InputNewPlay.Size = new System.Drawing.Size(110, 23);
+            this.InputNewPlay.TabIndex = 16;
+            this.InputNewPlay.Text = "Input Play Details";
+            this.InputNewPlay.UseVisualStyleBackColor = true;
+            this.InputNewPlay.Click += new System.EventHandler(this.InputNewPlay_Click);
+            // 
+            // AddBack
+            // 
+            this.AddBack.Location = new System.Drawing.Point(387, 287);
+            this.AddBack.Name = "AddBack";
+            this.AddBack.Size = new System.Drawing.Size(75, 23);
+            this.AddBack.TabIndex = 17;
+            this.AddBack.Text = "Back";
+            this.AddBack.UseVisualStyleBackColor = true;
+            this.AddBack.Click += new System.EventHandler(this.AddBack_Click);
+            // 
+            // PlaysForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 365);
+            this.Controls.Add(this.AddBack);
+            this.Controls.Add(this.InputNewPlay);
+            this.Controls.Add(this.TicketQuant);
+            this.Controls.Add(this.TicketsAvat);
+            this.Controls.Add(this.Timet);
+            this.Controls.Add(this.Datet);
+            this.Controls.Add(this.Descriptiont);
+            this.Controls.Add(this.TicketQuanl);
+            this.Controls.Add(this.TicketsAval);
+            this.Controls.Add(this.Timel);
+            this.Controls.Add(this.Datel);
+            this.Controls.Add(this.Genret);
+            this.Controls.Add(this.Descriptionl);
+            this.Controls.Add(this.Genrel);
+            this.Controls.Add(this.Titlet);
+            this.Controls.Add(this.TitleL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddPlay1);
+            this.Name = "PlaysForm";
+            this.Text = "Plays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddPlay1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TitleL;
+        private System.Windows.Forms.TextBox Titlet;
+        private System.Windows.Forms.Label Genrel;
+        private System.Windows.Forms.Label Descriptionl;
+        private System.Windows.Forms.TextBox Genret;
+        private System.Windows.Forms.Label Datel;
+        private System.Windows.Forms.Label Timel;
+        private System.Windows.Forms.Label TicketsAval;
+        private System.Windows.Forms.Label TicketQuanl;
+        private System.Windows.Forms.TextBox Descriptiont;
+        private System.Windows.Forms.TextBox Datet;
+        private System.Windows.Forms.TextBox Timet;
+        private System.Windows.Forms.TextBox TicketsAvat;
+        private System.Windows.Forms.TextBox TicketQuant;
+        private System.Windows.Forms.Button InputNewPlay;
+        private System.Windows.Forms.Button AddBack;
+    }
+}
