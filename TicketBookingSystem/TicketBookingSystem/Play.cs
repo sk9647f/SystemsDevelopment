5dd072197dea8e99a3dd9993e83781e9a63c34e4
sk9647f <36513930+sk9647f@users.noreply.github.com>
2019-03-27 12:35:56 +0000

    Add files via upload

--- TicketBookingSystem/TicketBookingSystem/Play.cs
+++ TicketBookingSystem/TicketBookingSystem/Play.cs
@@ -10,14 +10,17 @@ public class Play
 {
     int playID;
     public string title;
+    public int success;
+    public float checkValue;
     string genre;
     string description; // include duration of play
     string date;
-    public string time;
+    string time;
     //have to use datetime function to switch from a string into a formatted datetime
     string ticketsAvailable;
     string ticketsQuantity;
     public List <string> comboValue = new List<string>();
+    public List<string> timeBoxValue = new List<string>();
     //string[] array;
 
 
@@ -30,19 +33,20 @@ public class Play
 
 
 
-    public string DisplayReview()
+    public string DisplayReview(string Title)
     {
 
         string returnReview = "";
+        string addString = "";
         string connString;
-        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Comp-1632-System Development Project\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
 
         OleDbConnection myConnection = new OleDbConnection(connString);
         myConnection.Open();
 
 
-        OleDbCommand myCommand = new OleDbCommand("SELECT DISTINCT Title FROM Plays WHERE Title = @Title", myConnection);
-        //myCommand.Parameters.AddWithValue("@Title", input);
+        OleDbCommand myCommand = new OleDbCommand("SELECT DISTINCT Author, Review FROM Reviews WHERE Title = @Title", myConnection);
+        myCommand.Parameters.AddWithValue("@Title", Title);
 
 
 
@@ -50,11 +54,16 @@ public class Play
         while (reader.Read())
         {
             returnReview = reader.GetString(0);
+            addString += "Author  " + returnReview + ":   ";
+            returnReview = reader.GetString(1);
+            addString += "  " + returnReview + "" ;
+            addString += "\n";
+            addString += "\n";
 
         }
         reader.Close();
 
-        return returnReview;
+        return addString;
 
 
     }
@@ -66,7 +75,7 @@ public class Play
         string initialreturn = "";
         string addString = "";
         string connString;
-        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Comp-1632-System Development Project\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
 
         OleDbConnection myConnection = new OleDbConnection(connString);
         myConnection.Open();
@@ -82,7 +91,7 @@ public class Play
             addString += "Title: " + initialreturn + "  ";
             initialreturn = reader.GetDateTime(1).ToShortDateString();
             addString += "Date: " + initialreturn + "  ";
-            initialreturn = reader.GetDateTime(2).ToShortTimeString();
+            initialreturn = reader.GetString(2);
             addString += "Time: " + initialreturn + "\n";
             addString += "\n";
             addString += "\n";
@@ -105,28 +114,70 @@ public class Play
 
         string connString;
 
-        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Comp-1632-System Development Project\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+        OleDbConnection myConnection = new OleDbConnection(connString);
+        myConnection.Open();
+
+        if (titleInput != "")
+        {
+            OleDbCommand myCommand = new OleDbCommand("SELECT Title, DateofPlay, TimeofPlay FROM Plays WHERE Title = @InputTitle AND DateofPlay = @InputDate", myConnection);
+            myCommand.Parameters.AddWithValue("@InputTitle", titleInput);
+            myCommand.Parameters.AddWithValue("@InputDate", dateInput);
+            OleDbDataReader reader = myCommand.ExecuteReader();
+            while (reader.Read())
+            {
+
+
+
+                title = reader.GetString(0);
+                time = reader.GetString(2);
+
+
+
+                comboValue.Add(title);
+                timeBoxValue.Add(time);
+
+
+
+            }
+        }
+
+        
+
+
+        myConnection.Close();
+
+
+    }
+
+    public string searchGenre(string input)
+    {
+        string inputString = input;
+        string returnAllPlays = "";
+        string initialreturn = "";
+        string addString = "";
+        string connString;
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
         OleDbConnection myConnection = new OleDbConnection(connString);
         myConnection.Open();
 
 
-        OleDbCommand myCommand = new OleDbCommand("SELECT Title, DateofPlay, TimeofPlay FROM Plays WHERE Title = @InputTitle AND DateofPlay = @InputDate", myConnection);
-        myCommand.Parameters.AddWithValue("@InputTitle", titleInput);
-        myCommand.Parameters.AddWithValue("@InputDate", dateInput);
+        OleDbCommand myCommand = new OleDbCommand("SELECT Title, DateofPlay, TimeofPlay FROM Plays WHERE Genre = @Input", myConnection);
+        myCommand.Parameters.AddWithValue("@Input", input);
+
         OleDbDataReader reader = myCommand.ExecuteReader();
         while (reader.Read())
         {
+            initialreturn = reader.GetString(0);
 
-
-
-            title = reader.GetString(0);
-            time = reader.GetDateTime(2).ToShortTimeString();
-
-
-            
-            comboValue.Add(title + "" + time);
-
-
+            addString += "Title: " + initialreturn + "  ";
+            initialreturn = reader.GetDateTime(1).ToShortDateString();
+            addString += "Date: " + initialreturn + "  ";
+            initialreturn = reader.GetString(2);
+            addString += "Time: " + initialreturn + "\n";
+            addString += "\n";
+            addString += "\n";
 
 
         }
@@ -134,7 +185,175 @@ public class Play
 
         myConnection.Close();
 
+        returnAllPlays = addString;
 
+        return returnAllPlays;
+
+    }
+
+
+    public string SearchPlaysUpdate(string input, DateTime dateInput )
+    {
+        DateTime inputDateTime = dateInput;
+        string Inputstring = input;
+        string returnAllPlays = "";
+        string initialreturn = "";
+        string addString = "";
+        string connString;
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
+        OleDbConnection myConnection = new OleDbConnection(connString);
+        myConnection.Open();
+
+        if (title != "")
+        {
+            OleDbCommand myCommand = new OleDbCommand("SELECT Title, DateofPlay, TimeofPlay FROM Plays WHERE DateofPlay = @InputDate", myConnection);
+            
+            myCommand.Parameters.AddWithValue("@InputDate", inputDateTime);
+
+            OleDbDataReader reader = myCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                initialreturn = reader.GetString(0);
+
+                addString += "Title: " + initialreturn + "  ";
+                initialreturn = reader.GetDateTime(1).ToShortDateString();
+                addString += "Date: " + initialreturn + "  ";
+                initialreturn = reader.GetString(2);
+                addString += "Time: " + initialreturn + "\n";
+                addString += "\n";
+                addString += "\n";
+
+
+            }
+        }
+
+        
+        else
+        {
+            OleDbCommand myCommand = new OleDbCommand("SELECT Title, DateofPlay, TimeofPlay FROM Plays WHERE Title = @InputTitle AND DateOfPlay = @Iputdate", myConnection);
+            myCommand.Parameters.AddWithValue("@InputTitle", Inputstring);
+            myCommand.Parameters.AddWithValue("@InputDate", inputDateTime);
+
+            OleDbDataReader reader = myCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                initialreturn = reader.GetString(0);
+
+                addString += "Title: " + initialreturn + "  ";
+                initialreturn = reader.GetDateTime(1).ToShortDateString();
+                addString += "Date: " + initialreturn + "  ";
+                initialreturn = reader.GetString(2);
+                addString += "Time: " + initialreturn + "\n";
+                addString += "\n";
+                addString += "\n";
+
+
+            }
+        }
+
+        myConnection.Close();
+
+        returnAllPlays = addString;
+
+        //AllPlaysl.Text = returnAllPlays;
+        return returnAllPlays;
+
+    }
+
+    public void AddtoBasket(string userID, string PlayChosen, float Standard, float Child, float OAP, DateTime PlayDate, string Time, string Price, float Quantity)
+    {
+
+
+        
+        string checkString;
+        float checkValue;
+        //needs to write to database 
+        string connString;
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
+        OleDbConnection myConnection = new OleDbConnection(connString);
+        myConnection.Open();
+
+        OleDbCommand checkCommand = new OleDbCommand("SELECT TicketsQuantity FROM Plays WHERE Title = @Title AND DateOfPlay = @DateOfPlay AND TimeOfPlay = @TimeOfPlay", myConnection);
+
+        checkCommand.Parameters.AddWithValue("@Title", PlayChosen);
+        checkCommand.Parameters.AddWithValue("@DateOfPlay", PlayDate);
+        checkCommand.Parameters.AddWithValue("@TimeOfPlay", Time);
+        
+        
+        OleDbDataReader reader2 = checkCommand.ExecuteReader();
+        
+        while (reader2.Read())
+        {
+            checkString = reader2.GetString(0);
+            checkValue = Convert.ToInt32(checkString);
+            
+            if ((checkValue - Quantity) >= 0)
+            {
+                OleDbCommand myCommand = new OleDbCommand("INSERT INTO [Basket] (UserID, PlayChosen, StandardNo, ChildNo, OAPNo, PlayDate, PlayTime, Price, Quantity) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", myConnection);
+
+                myCommand.Parameters.AddWithValue("@UserID", userID);
+                myCommand.Parameters.AddWithValue("@PlayChosen", PlayChosen);
+                myCommand.Parameters.AddWithValue("@Standard", Standard);
+                myCommand.Parameters.AddWithValue("@Child", Child);
+                myCommand.Parameters.AddWithValue("@OAP", OAP);
+                myCommand.Parameters.AddWithValue("@PlayDate", PlayDate);
+                myCommand.Parameters.AddWithValue("@PlayTime", Time);
+                myCommand.Parameters.AddWithValue("@Price", Price);
+                myCommand.Parameters.AddWithValue("@Quantity", Quantity);
+
+                
+                myCommand.ExecuteNonQuery();
+                
+
+                success = 1;
+
+
+
+                OleDbCommand decrementCommand = new OleDbCommand("UPDATE Plays SET TicketsQuantity = @TicketsQuantity WHERE Title = @Title AND DateOfPlay = @DateOfPlay AND TimeOfPlay = @TimeOfPlay", myConnection);
+                decrementCommand.Parameters.AddWithValue("@TicketsQuantity", (checkValue - Quantity));
+                decrementCommand.Parameters.AddWithValue("@Title", PlayChosen);
+                decrementCommand.Parameters.AddWithValue("@DateOfPlay", PlayDate);
+                decrementCommand.Parameters.AddWithValue("@TimeOfPlay", Time);
+                decrementCommand.ExecuteNonQuery();
+                
+            }
+
+            else
+            {
+                success = 2;
+                
+            }
+
+            
+
+
+        }
+        myConnection.Close();
+       
+    }
+
+    public void addReview(string UserID, string Review, string Title)
+    {
+        string checkString;
+        float checkValue;
+        //needs to write to database 
+        string connString;
+        connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+
+        OleDbConnection myConnection = new OleDbConnection(connString);
+        myConnection.Open();
+
+
+
+        OleDbCommand myCommand = new OleDbCommand("INSERT INTO Reviews (Author, Review, Title) VALUES (?, ?, ?)", myConnection);
+        myCommand.Parameters.AddWithValue("@Author", UserID);
+        myCommand.Parameters.AddWithValue("@Review", Review);
+        myCommand.Parameters.AddWithValue("@Title", Title);
+        
+
+        myCommand.ExecuteNonQuery();
     }
 
 
--- TicketBookingSystem/TicketBookingSystem/PlaysForm.cs
+++ TicketBookingSystem/TicketBookingSystem/PlaysForm.cs
@@ -14,6 +14,8 @@ namespace TicketBookingSystem
 {
     public partial class PlaysForm : Form
     {
+
+        
         public PlaysForm()
         {
             Play plays = new Play();
@@ -21,10 +23,15 @@ namespace TicketBookingSystem
             InitializeComponent();
 
             AllPlaysl.Text = plays.DisplayPlays();
+            
         }
 
 
         int playID;
+        float currTotal;
+        int DayInt;
+
+
         string oldTitle;
         string title;
         string genre;
@@ -44,15 +51,15 @@ namespace TicketBookingSystem
             plays.SearchPlays(titleInput, dateInput);
 
 
-
+            TimeBox.DataSource = plays.timeBoxValue;
             comboBox1.DataSource = plays.comboValue;
-            
+
 
             /*string returnAllPlays = "";
             string initialreturn = "";
             string addString = "";
             string connString;
-            connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Comp-1632-System Development Project\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
+            connString = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source = L:\Year 2\Systems Development\Coursework\SystemsDevelopment-master\TicketBookingSystem\TicketBookingSystem\TicketSysDB.mdb";
 
 
             OleDbConnection myConnection = new OleDbConnection(connString);
@@ -88,7 +95,7 @@ namespace TicketBookingSystem
 
         }
 
-      
+
 
         private void AddBack_Click(object sender, EventArgs e)
         {
@@ -110,16 +117,32 @@ namespace TicketBookingSystem
 
             AllPlaysl.Visible = true;
 
+            panel1.Visible = false;
 
+            TimeBox.Visible = false;
+
+            SearchGenreb.Visible = true;
+
+            SearchGenrel.Visible = true;
+
+            comboBox2.Visible = true;
+
+            playTitleTextBox.Visible = true;
+
+            PlayTitlel.Visible = true;
+
+            ReviewTextBox.Visible = false;
+
+            ReviewsWriteB.Visible = false;
+
+            ReviewsReadb.Visible = false;
+
+            ConfirmReview.Visible = false;
 
         }
 
       
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Play plays = new Play();
-            plays.DisplayReview();
-        }
+        
 
         private void PlaysForm_Load(object sender, EventArgs e)
         {
@@ -138,6 +161,9 @@ namespace TicketBookingSystem
         {
             //comboBox1.Items.Clear();
             SearchPlays();
+            Play plays = new Play();
+
+            AllPlaysl.Text = plays.SearchPlaysUpdate(playTitleTextBox.Text, dateTimePicker2.Value);
 
             AddBack.Visible = true;
 
@@ -146,6 +172,9 @@ namespace TicketBookingSystem
             BookTickB.Visible = true;
 
             comboBox1.Visible = true;
+            TimeBox.Visible = true;
+            ReviewsReadb.Visible = true;
+            ReviewsWriteB.Visible = true;
         }
 
         private void BookTickB_Click(object sender, EventArgs e)
@@ -160,6 +189,321 @@ namespace TicketBookingSystem
         }
 
 
+        private void BookTickB_Click_1(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            AllPlaysl.Visible = false;
+            SearchDateB.Visible = false;
+            playTitleTextBox.Visible = false;
+            PlayTitlel.Visible = false;
+            SearchGenreb.Visible = false;
+            SearchGenrel.Visible = false;
+            dateTimePicker2.Visible = false;
+            comboBox1.Visible = false;
+            comboBox2.Visible = false;
+            BookTickB.Visible = false;
+            PlayDatel.Visible = false;
+            ReviewsReadb.Visible = false;
+            DayInt = (int)dateTimePicker2.Value.DayOfWeek;
+            if (DayInt <= 4)
+            {
+                StandardPl.Text = "£18";
+                ChildPl.Text = "£13.50";
+                OAPPl.Text = "£9";
+            }
+            else
+            {
+                StandardPl.Text = "£20";
+                ChildPl.Text = "£15";
+                OAPPl.Text = "£10";
+            }
+            
+            CurrTotal.Text = "Current Total: £ ";
+            BookTitlel.Text = playTitleTextBox.Text;
+            BookDatel.Text = dateTimePicker2.Text;
+            BookTimel.Text = TimeBox.Text;
+
+            
+        }
+
+        private void RadioBandB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DayInt < 4)
+            {
+                float Stand = Convert.ToInt32(StandardUpDn.Value);
+                float Child = Convert.ToInt32(ChildUpDn.Value);
+                float OAP = Convert.ToInt32(OAPUpDn.Value);
+                if (RadioBandA.Checked)
+                {
+                    currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandB.Checked)
+                {
+                    currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandC.Checked)
+                {
+                    currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandB.Checked == true)
+                {
+                    StandardPl.Text = "£WORKING" + "";
+                    ChildPl.Text = "£10";
+                    OAPPl.Text = "£5";
+                }
+            }
+            else
+            {
+                float Stand = Convert.ToInt32(StandardUpDn.Value);
+                float Child = Convert.ToInt32(ChildUpDn.Value);
+                float OAP = Convert.ToInt32(OAPUpDn.Value);
+                if (RadioBandA.Checked)
+                {
+                    currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandB.Checked)
+                {
+                    currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandC.Checked)
+                {
+                    currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                    CurrTotal.Text = "£ " + currTotal;
+                }
+                if (RadioBandB.Checked == true)
+                {
+                    StandardPl.Text = "£15";
+                    ChildPl.Text = "£10";
+                    OAPPl.Text = "£5";
+                }
+            }
+        }
+
+        private void RadioBandA_CheckedChanged(object sender, EventArgs e)
+        {
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            if (RadioBandA.Checked)
+            {
+                currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandB.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandA.Checked == true)
+            {
+                StandardPl.Text = "£20";
+                ChildPl.Text = "£15";
+                OAPPl.Text = "£10";
+            }
+        }
+
+        private void RadioBandC_CheckedChanged(object sender, EventArgs e)
+        {
+
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            if (RadioBandA.Checked)
+            {
+                currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandB.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked == true)
+            {
+                StandardPl.Text = "£10";
+                ChildPl.Text = "£5";
+                OAPPl.Text = "£5";
+            }
+        }
+
+        private void StandardUpDn_ValueChanged(object sender, EventArgs e)
+        {
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            if (RadioBandA.Checked)
+            {
+                currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandB.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+        }
+
+        private void ChildUpDn_ValueChanged(object sender, EventArgs e)
+        {
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            if (RadioBandA.Checked)
+            {
+                currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandB.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+        }
+
+        private void OAPUpDn_ValueChanged(object sender, EventArgs e)
+        {
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            if (RadioBandA.Checked)
+            {
+                currTotal = (Stand * 20) + (Child * 15) + (OAP * 10);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandB.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 10) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+            if (RadioBandC.Checked)
+            {
+                currTotal = (Stand * 15) + (Child * 5) + (OAP * 5);
+                CurrTotal.Text = "£ " + currTotal;
+            }
+        }
+
+        private void BookConfb_Click(object sender, EventArgs e)
+        {
+            float Stand = Convert.ToInt32(StandardUpDn.Value);
+            float Child = Convert.ToInt32(ChildUpDn.Value);
+            float OAP = Convert.ToInt32(OAPUpDn.Value);
+            float Quantity = Stand + Child + OAP;
+            
+            Play plays = new Play();
+            CheckValuel.Text = plays.checkValue.ToString();
+            plays.AddtoBasket("User1", BookTitlel.Text, Stand, Child, OAP, dateTimePicker2.Value,BookTimel.Text, CurrTotal.Text, Quantity);
+            if (plays.success == 1){
+
+                MessageBox.Show("Your tickets were booked successfully");
+                CheckValuel.Text = plays.checkValue.ToString();
+
+                
+
+            }
+            else if(plays.success == 2)
+            {
+                MessageBox.Show("You booked more seats than are available");
+                CheckValuel.Text = plays.checkValue.ToString();
+            }
+            StandardUpDn.Value = 0;
+            ChildUpDn.Value = 0;
+            OAPUpDn.Value = 0;
+
+
+
+        }
+
+        private void ReviewsWriteB_Click(object sender, EventArgs e)
+        {
+            ReviewsWriteB.Visible = false;
+            ReviewTextBox.Visible = true;
+            ConfirmReview.Visible = true;
+
+            ReviewsReadb.Visible = false;
+
+            panel1.Visible = false;
+
+            comboBox1.Visible = false;
+
+            comboBox2.Visible = false;
+
+            PlayTitlel.Visible = false;
+
+            PlayDatel.Visible = false;
+
+            playTitleTextBox.Visible = false;
+
+            dateTimePicker2.Visible = false;
+
+            TimeBox.Visible = false;
+
+            BookTickB.Visible = false;
+
+            SearchDateB.Visible = false;
+
+            SearchGenreb.Visible = false;
+
+            SearchGenrel.Visible = false;
+
+            AllPlaysl.Visible = false;
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Play plays = new Play();
+            AllPlaysl.Text = plays.DisplayReview(comboBox1.Text);
+        }
+
+
+
+        private void SearchGenreb_Click(object sender, EventArgs e)
+        {
+            Play plays = new Play();
+
+            AllPlaysl.Text = plays.searchGenre(comboBox2.Text);
+
+        }
+
+        private void ConfirmReview_Click(object sender, EventArgs e)
+        {
+            Play plays = new Play();
+            if (ReviewTextBox.Text != "")
+            {
+                plays.addReview("User1", ReviewTextBox.Text, comboBox1.Text);
+                ReviewTextBox.Clear();
+            }
+            if (ReviewTextBox.Text == "")
+            {
+                MessageBox.Show("The textbox can't be left empty for a review");
+            }
+        }
 
 
         //Using both the title and its date as an identifier 
